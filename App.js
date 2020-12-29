949a7e906b40ce16c3b5f48127b6f9f8fb0eb9e8
Anas-Aqeel <71166989+Anas-Aqeel@users.noreply.github.com>
2020-12-29 17:24:04 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+function str() {
+ alert("hello")
+}
+//var lavel = prompt("Enter Your Mind Lavel")
+//var num = Math.random() * lavel;
+//var num1 = Math.floor(num);
+//docoment.write("hello world")
